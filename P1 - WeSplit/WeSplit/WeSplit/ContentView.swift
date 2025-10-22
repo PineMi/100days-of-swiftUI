eae47739ef41e40e86f10285446ac261bfae91bc
Miguel Piñeiro <pineiro.c.s.mi@gmail.com>
2025-10-22 17:26:36 -0300

    Forms and Sections

--- P1 - WeSplit/WeSplit/WeSplit/ContentView.swift
+++ P1 - WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello World")
+                }
+                
+                Section {
+                    Text("Hello World")
+                }
+            }
         }
-        .padding()
     }
 }
 
