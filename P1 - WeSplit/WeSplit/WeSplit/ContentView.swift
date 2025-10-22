e464234d553ba2f1090d0acc2f3c8ca5140e7d1f
Miguel Piñeiro <pineiro.c.s.mi@gmail.com>
2025-10-22 19:09:21 -0300

    WeSplit (Project 1)

--- P1 - WeSplit/WeSplit/WeSplit/ContentView.swift
+++ P1 - WeSplit/WeSplit/WeSplit/ContentView.swift
@@ -8,19 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = "Harry"
-    let students: [String] = ["Harry", "Hermione", "Ron"]
+    @State private var numPeople = 0
+    @State private var bill = 0.0
+    @State private var tipPercentage = 10
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = bill / 100 * tipSelection
+        let grandTotal = bill + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
+    
+    let tipRecommendations = [0, 10, 15, 20, 25]
     var body: some View {
         NavigationStack {
             Form {
-                Picker("Select your favourite Student", selection: $name) {
-                    ForEach(students, id:\.self) {
-                        Text($0)
+                
+                Section {
+                    TextField("Amount", value:$bill, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of People", selection:$numPeople){
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
                     }
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Select your tip amout", selection: $tipPercentage) {
+                        ForEach(tipRecommendations, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Amount per Person") {
+                    Text(totalPerPerson, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
                 }
             }
         }
-        .navigationTitle("Select Student")
+        .navigationTitle("WeSplit")
     }
 }
 
