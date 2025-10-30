1f253d61a8075105af5773d48270ef43e94e361f
Miguel Piñeiro <pineiro.c.s.mi@gmail.com>
2025-10-29 22:36:50 -0300

    Project6 - start - animations

---
+++ Animations/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Miguel Piñeiro on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/Animations/Animations/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Miguel Piñeiro on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var animationAmount = 1.0
+    var body: some View {
+        Button("Tap Me") {
+            if animationAmount <= 4 {
+                animationAmount += 0.5
+            } else {
+                animationAmount = 1
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .animation(.default, value: animationAmount)
+    }
+}
+
+#Preview {
+    ContentView()
+}
